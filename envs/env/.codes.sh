24cd6ea86dc2e4c64b9379c125b6c95add5c7737
晴风 <773939719@qq.com>
2025-02-08 21:28:47 +0800

    update

--- envs/env/.codes.sh
+++ envs/env/.codes.sh
@@ -2,3 +2,7 @@ alias WebKit="cd && qCodes && cd Apple/WebKit"
 alias Swift="cd && qCodes && cd Apple/Swift"
 alias V8="cd && qCodes && cd V8"
 alias Unreal="cd && qCodes && cd Unreal"
+alias Flutter="cd && qCodes && cd Flutter/engine"
+alias FireFox="cd && qCodes && cd FireFox/FireFox"
+
+alias FastClient="gclient sync --with_branch_heads --with_tags --no-history -v"
