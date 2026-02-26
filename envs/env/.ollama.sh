4df635d51b0c9e8c206f349a68c6767501c2a02f
晴风 <773939719@qq.com>
2026-02-26 09:59:26 +0800

    update

--- envs/env/.ollama.sh
+++ envs/env/.ollama.sh
@@ -1,33 +1,36 @@
 # https://docs.ollama.com/
 # --config | --model
-# qwen3-coder:30b                                        06c1097efce0    18 GB     10 minutes ago
-# qwen3-coder:latest                                     06c1097efce0    18 GB     55 minutes ago
-# gpt-oss:latest                                         17052f91a42e    13 GB     55 minutes ago
-# deepseek-r1:latest                                     6995872bfe4c    5.2 GB    55 minutes ago
-# deepseek-coder-v2:latest                               63fb193b3a9b    8.9 GB    55 minutes ago
-# deepseek-coder:latest                                  3ddd2d3fc8d2    776 MB    55 minutes ago
-# gemma3:12b-cloud                                       485e7119a53a    -         5 hours ago
-# ministral-3:14b-cloud                                  615c59440878    -         5 hours ago
-# qwen3-vl:235b-cloud                                    86b3322ec200    -         5 hours ago
-# qwen3-coder-next:cloud                                 aa626c11ae8d    -         5 hours ago
-# qwen3.5:cloud                                          a7bf6f7891c3    -         5 hours ago
-# deepseek-v3.1:671b-cloud                               d3749919e45f    -         5 hours ago
-# gpt-oss:120b-cloud                                     569662207105    -         5 hours ago
-# minimax-m2.5:cloud                                     c0d5751c800f    -         5 hours ago
-# minimax-m2:cloud                                       698ab6d56142    -         5 hours ago
-# kimi-k2.5:cloud                                        6d1c3246c608    -         5 hours ago
-# glm-4.7:cloud                                          023608864819    -         5 hours ago
-# glm-5:cloud                                            c313cd065935    -         5 hours ago
-# hf.co/unsloth/DeepSeek-R1-0528-Qwen3-8B-GGUF:Q4_K_M    ecc092d5e10a    5.0 GB    12 days ago
-# hf.co/unsloth/gpt-oss-20b-GGUF:Q4_K_M                  779e997d7f03    11 GB     12 days ago
-# ministral-3:latest                                     1922accd5827    6.0 GB    2 weeks ago
-# openbmb/minicpm-v4.5:latest                            d60b490c2110    5.9 GB    2 weeks ago
-# glm-4.7-flash:latest                                   d1a8a26252f1    19 GB     2 weeks ago
-# llama3:latest                                          365c0bd3c000    4.7 GB    2 weeks ago
+# NAME                                                   ID              SIZE      MODIFIED
+# gemma3:12b-cloud                                       485e7119a53a    -         8 hours ago
+# ministral-3:14b-cloud                                  615c59440878    -         8 hours ago
+# qwen3-vl:235b-cloud                                    86b3322ec200    -         8 hours ago
+# qwen3-coder-next:cloud                                 aa626c11ae8d    -         8 hours ago
+# qwen3.5:cloud                                          a7bf6f7891c3    -         8 hours ago
+# deepseek-v3.1:671b-cloud                               d3749919e45f    -         8 hours ago
+# gpt-oss:120b-cloud                                     569662207105    -         8 hours ago
+# minimax-m2.5:cloud                                     c0d5751c800f    -         8 hours ago
+# minimax-m2:cloud                                       698ab6d56142    -         8 hours ago
+# kimi-k2.5:cloud                                        6d1c3246c608    -         8 hours ago
+# glm-4.7:cloud                                          023608864819    -         8 hours ago
+# glm-5:cloud                                            c313cd065935    -         8 hours ago
+# bazobehram/qwen3.5-flash-27b:latest                    6cc996efdb6d    16 GB     8 hours ago
+# hf.co/unsloth/gpt-oss-20b-GGUF:Q4_K_M                  779e997d7f03    11 GB     8 hours ago
+# hf.co/unsloth/DeepSeek-R1-0528-Qwen3-8B-GGUF:Q4_K_M    ecc092d5e10a    5.0 GB    8 hours ago
+# openbmb/minicpm-v4.5:latest                            d60b490c2110    5.9 GB    8 hours ago
+# openbmb/minicpm-o4.5:latest                            e900f7386920    6.1 GB    8 hours ago
+# ministral-3:latest                                     1922accd5827    6.0 GB    8 hours ago
+# glm-4.7-flash:latest                                   d1a8a26252f1    19 GB     8 hours ago
+# llama3:latest                                          365c0bd3c000    4.7 GB    8 hours ago
+# qwen3.5:35b                                            55ff1d561367    23 GB     8 hours ago
+# qwen3-coder:latest                                     06c1097efce0    18 GB     9 hours ago
+# gpt-oss:latest                                         17052f91a42e    13 GB     9 hours ago
+# deepseek-r1:latest                                     6995872bfe4c    5.2 GB    9 hours ago
+# deepseek-coder-v2:latest                               63fb193b3a9b    8.9 GB    9 hours ago
+# deepseek-coder:latest                                  3ddd2d3fc8d2    776 MB    9 hours ago
+# qwen3-coder:30b                                        06c1097efce0    18 GB     10 hours ago
 # qwen3:8b                                               500a1f067a9f    5.2 GB    2 weeks ago
 # gpt-oss:20b-cloud                                      875e8e3a629a    -         3 weeks ago
 # deepseek-r1:8b                                         6995872bfe4c    5.2 GB    3 weeks ago
-
 alias ollama_claude="ollama launch claude"
 alias ollama_codex="ollama launch codex"
 alias ollama_opencode="ollama launch opencode"
